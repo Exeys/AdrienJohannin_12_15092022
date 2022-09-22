480d6d123c546d14701d7fee344026ea1d4a1852
Adrien <47904625+Exeys@users.noreply.github.com>
2022-09-22 15:14:30 +0200

    header integration

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Header.css'
+import logo from '../../assets/logo.png'
+import logoText from '../../assets/logoText.png'
+
+const Header = () => {
+    return (
+        <nav className="HeaderContainer">
+            <a className="HeaderItem" href=".">
+                <img src={logo} alt="logo" />
+                <img src={logoText} alt="logo" height={24.29} width={109.3}/>
+            </a>
+            <a className="HeaderItem" href=".">Accueil</a>
+            <a className="HeaderItem" href=".">Profil</a>
+            <a className="HeaderItem" href=".">Réglage</a>
+            <a className="HeaderItem" href=".">Communauté</a>
+        </nav>
+    )
+}
+
+export default Header;
---
+++ src/views/Welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Welcome.css'
+import Header from '../../components/Header/Header.js'
+
+const Welcome = () => {
+    return(
+        <Header />
+    )
+
+}
+
+export default Welcome;
